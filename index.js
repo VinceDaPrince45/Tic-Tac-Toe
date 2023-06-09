d89013bab5e0d7b94bcd446a3ee3b33e5c962bd1
Vincent <Vincentnguyen030@gmail.com>
2023-06-08 22:02:08 -0500

    Create function for gameClock and evaluateRows

--- index.js
+++ index.js
@@ -15,14 +15,45 @@ const playGame = (() => {
     // placholder to get game to work
     let playerOneMarker = 'X';
     let playerTwoMarker = '0'
+    // gameClock
+});
+
+// function to choose name and marker
+const Player = (playerName,playerMarker) => {
+    return {name:playerName, marker:playerMarker};
+} 
+
+// function to choose spot on board
+function chooseSpot(spot,marker) {
+    spot.textContent = marker;    
+}
+
+
+// function to keep game running and laternate turns
+const gameClock = (() => {
+    let counter = 0;
+    game = true;
     while (game) {
-        if (counter % 2 === 0) {
-            // player 1 turn
-        } else if (counter % 2 !== 0) {
-            // player 2 turn
+        if (counter%2 == 0) {
+        // if counter is even let one player play
+        } else if (counter%2 !== 0) {
+        // if counter odd let other player play
         }
         // function to update board
         // function to evaluate who wins
     }
+});
 
-})
+function evaluateRows(gameBoard) {
+    let counter = 0;
+    for (const grid of gameboard) {
+        if (grid == 'X') || (grid == 'O') {
+            counter += 1;
+        }
+    }
+    if (counter == 9) {
+        return 'TIE'
+    } else if ((gameBoard[0] ==  gameBoard[1] == gameBoard[2] !== '') || (gameBoard[3] == gameBoard[4] == gameBoard[5] !== '') || (gameBoard[6] ==  gameBoard[7] == gameBoard[8] !== '') || (gameBoard[0] ==  gameBoard[3] == gameBoard[6] !== '') || (gameBoard[1] ==  gameBoard[4] == gameBoard[7] !== '') || (gameBoard[2] ==  gameBoard[5] == gameBoard[8] !== '') || (gameBoard[0] ==  gameBoard[4] == gameBoard[8] !== '') || (gameBoard[2] ==  gameBoard[4] == gameBoard[6] !== '')) {
+        return 'Winner';
+    }   
+}
