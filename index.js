fffaf5130c240d3f63d771d2d1c8574200c89659
Vincent <Vincentnguyen030@gmail.com>
2023-06-09 19:39:50 -0500

    Move while function to within eventlistener

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-function evaluateRows(gameBoard) {
-    let counter = 0;
-    for (const grid of gameboard) {
-        if ((grid == 'X') || (grid == 'O')) {
-            counter += 1;
-        }
-    }
-    if (counter == 9) {
-        return 'TIE'
-    } else if ((gameBoard[0] ==  gameBoard[1] == gameBoard[2] !== '') || (gameBoard[3] == gameBoard[4] == gameBoard[5] !== '') || (gameBoard[6] ==  gameBoard[7] == gameBoard[8] !== '') || (gameBoard[0] ==  gameBoard[3] == gameBoard[6] !== '') || (gameBoard[1] ==  gameBoard[4] == gameBoard[7] !== '') || (gameBoard[2] ==  gameBoard[5] == gameBoard[8] !== '') || (gameBoard[0] ==  gameBoard[4] == gameBoard[8] !== '') || (gameBoard[2] ==  gameBoard[4] == gameBoard[6] !== '')) {
-        return 'Winner';
-    }   
-}
-
-
 // playerOne = Player('PlayerOne','X')
 // playerTwo = Player('PlayerTwo','O')
 // playGame.initializeBoard (creates board/array)
@@ -28,7 +13,7 @@ const Player = (playerName,playerMarker) => {
 }
 
 const playerOne = Player('playerOne','X');
-const playerTwo = Player('playerTwo','O')
+const playerTwo = Player('playerTwo','O');
 
 const playGame = ((playerOne,playerTwo) => {
     let gameBoard = [
@@ -36,52 +21,82 @@ const playGame = ((playerOne,playerTwo) => {
         '', '', '',
         '', '', ''     
     ]
+    let game = true;
     const spots = document.querySelectorAll('.spot');
 
     const initializeBoard = () => {
         let counter = 0;
         spots.forEach((spot) => {
             spot.setAttribute('data-name',counter);
-            spot.textContent = array[counter];
+            spot.textContent = gameBoard[counter];
             counter += 1;
         });
     };
 
+    
     const evaluateRows = (array) => {
-        let counter = 0;
-        for (const grid of array) {
-            if ((grid == 'X') || (grid == 'O')) {
-                counter += 1;
-            }
-        }
-        if (counter == 9) {
-            return 'TIE'
-        } else if ((array[0] ==  array[1] == array[2] !== '') || (array[3] == array[4] == array[5] !== '') || (array[6] ==  array[7] == array[8] !== '') || (array[0] ==  array[3] == array[6] !== '') || (array[1] ==  array[4] == array[7] !== '') || (array[2] ==  array[5] == array[8] !== '') || (array[0] ==  array[4] == array[8] !== '') || (array[2] ==  array[4] == array[6] !== '')) {
-            return 'Winner';
+        if ((array[0] ==  array[1] == (array[2] !== '')) || (array[3] == array[4] == (array[5] !== '')) || (array[6] ==  array[7] == (array[8] !== '')) || (array[0] ==  array[3] == (array[6] !== '')) || (array[1] ==  array[4] == (array[7] !== '')) || (array[2] ==  array[5] == (array[8] !== '')) || (array[0] ==  array[4] == (array[8] !== '')) || (array[2] ==  array[4] == (array[6] !== ''))) {
+            game = false;
+            console.log(gameBoard);
+            console.log('WINNER');
+        } else if (array.includes('')) {
+            console.log('IN GAME');
+        } else {
+            game = false;
+            console.log('TIE');
         }
     };
 
-    const chooseSpot = (marker) => {
-        spots.forEach((spot) => {
-            spot.onclick = function(e) {
-                if (e.target && e.target.classList.contains('spot')) {
-                    gameBoard[e.target.getAttribute('data-name')] = marker;
+    const chooseSpot = (playerOne,playerTwo) => {
+        let counter = 0;
+        let currentMarker;
+        const container = document.querySelector('.container')
+        container.addEventListener('click', function(e) {
+            while (game) {
+                if (counter % 2 == 0) {
+                    currentMarker = playerOne.marker;
+                    if (e.target && e.target.classList.contains('spot')) {
+                        gameBoard[e.target.getAttribute('data-name')] = currentMarker;
+                        updateBoard();
+                        evaluateRows(gameBoard);
+                        
+                    }
+                    counter += 1;
+                } else if (counter % 2 !== 0) {
+                    currentMarker = playerTwo.marker;
+                    if (e.target && e.target.classList.contains('spot')) {
+                        gameBoard[e.target.getAttribute('data-name')] = currentMarker;
+                        updateBoard();
+                        evaluateRows(gameBoard);
+                    }
+                    counter += 1;
                 }
             }
+        })
+    };
+
+    const updateBoard = () => {
+        let counter = 0;
+        spots.forEach((spot) => {
+            spot.setAttribute('data-name',counter);
+            spot.textContent = gameBoard[counter];
+            counter += 1;
         });
     };
 
-    const gameClock = () => {
-        let counter = 0;
-        game = true;
-        while (game) {
-            if (counter % 2 == 0) {
-                // player 1 turn
-            } else if (counter % 2 !== 0) {
-                // player 2 turn
-            }
-        }
+    const gameClock = (playerOne,playerTwo) => {
     };
+
+    return {initializeBoard,chooseSpot}
 })();
 
-playGame.chooseSpot;
+// playGame.initializeBoard();
+// playGame.chooseSpot(playerOne,playerTwo);
+
+
+let gameBoard = [
+    '', 'X', '',
+    '', '', '',
+    '', '', ''     
+]
+console.log(gameBoard[0] == gameBoard == [1] == gameBoard[2] !== ' ');
